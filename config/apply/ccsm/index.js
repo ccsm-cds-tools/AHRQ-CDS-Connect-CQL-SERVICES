376e6166405415c1a0d31b65720f2af8c6a62a60
dwinters <dwinters@mitre.org>
2022-12-02 15:11:04 -0500

    Adding prefetch to configuration.

--- config/apply/ccsm/index.js
+++ config/apply/ccsm/index.js
@@ -1,3 +1,4 @@
 export { elmJsonDependencies as elmJson } from './cql/index.mjs';
 export { cdsResources } from './fhir/index.js';
 export { valueSetJson } from './valuesets/index.js';
+export { prefetch } from './prefetch.js';
