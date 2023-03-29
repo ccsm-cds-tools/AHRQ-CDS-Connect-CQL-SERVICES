cab984f87a2853ce425055989436e57bb3be53c8
dwinters <dwinters@mitre.org>
2023-03-29 12:38:55 -0400

    Adding AHRQ copyright to original cql-services files.

--- bin/www.js
+++ bin/www.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+// Portions of this file are based on the original [CQL Services](https://github.com/AHRQ-CDS/AHRQ-CDS-Connect-CQL-SERVICES) 
+// developed for the [CDS Connect](https://cds.ahrq.gov/cdsconnect) project, sponsored by the 
+// [Agency for Healthcare Research and Quality](https://www.ahrq.gov/) (AHRQ), and developed under contract with AHRQ by 
+// [MITRE's Health FFRDC](https://www.mitre.org/our-impact/rd-centers/health-ffrdc).
+// Copyright 2016-2018 Agency for Healthcare Research and Quality.
+// Licensed under the Apache License, Version 2.0 (the "License").
+
 'use strict';
 
 /* eslint-disable no-console */
--- routes/cds-services.js
+++ routes/cds-services.js
@@ -1,3 +1,9 @@
+// Portions of this file are based on the original [CQL Services](https://github.com/AHRQ-CDS/AHRQ-CDS-Connect-CQL-SERVICES) 
+// developed for the [CDS Connect](https://cds.ahrq.gov/cdsconnect) project, sponsored by the 
+// [Agency for Healthcare Research and Quality](https://www.ahrq.gov/) (AHRQ), and developed under contract with AHRQ by 
+// [MITRE's Health FFRDC](https://www.mitre.org/our-impact/rd-centers/health-ffrdc).
+// Copyright 2016-2018 Agency for Healthcare Research and Quality.
+// Licensed under the Apache License, Version 2.0 (the "License").
 'use strict';
 import { env } from 'process';
 import { Router } from 'express';
