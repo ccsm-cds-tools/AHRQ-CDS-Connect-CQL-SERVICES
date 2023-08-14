2029417ee72167f46b24ede38f7e2602acb2e18f
Mick O'Hanlon <mohanlon@mitre.org>
2023-08-14 11:09:27 -0400

    Add test to show that AIS and Histologic Cancer excision results map to a cervical biopsy diagnostic report, and that other excision results do not map to anything.

--- test/translate-test.js
+++ test/translate-test.js
@@ -10,16 +10,62 @@ describe('translateResponse', () => {
   beforeEach(() => {
     patientData = JSON.parse(patientDataContent).patientData;
     customApiResponse = JSON.parse(resultContent);
-    translateResponse(customApiResponse, patientData);
   });
 
   describe('parse procedure data', () => {
     it('should include Cervix Excision procedure', function () {
+      translateResponse(customApiResponse, patientData);
       expect(patientData.length).to.be.greaterThan(2);
 
       const procedures = patientData.filter(pd => pd.resourceType === 'Procedure');
-      expect(procedures.length).to.be.greaterThan(0);
+      expect(procedures.length).to.be.greaterThan(1);
       expect(procedures.some(p => p.code.coding.some(coding => coding.code === '120038005'))).to.be.true;
+    });
+  });
+
+  describe('Parse excision results that are not AIS or histologic cancer', () => {
+    it('should ignore excision results that do not show AIS or histologic cancer', function () {
+      translateResponse(customApiResponse, patientData);
+      const diagnosticReports = patientData.filter(pd => pd.resourceType === 'DiagnosticReport');
+
+      expect(diagnosticReports.length).to.equal(1);
+      expect(diagnosticReports.some(p => p.code.coding.some(coding => coding.code === '65753-6'))).to.be.false;
+    });
+  });
+
+  describe('Parse AIS excision results', () => {
+    it('should map an AIS excision result to a biopsy', function () {
+      customApiResponse.Order[0].ExcisionResults = 
+        [
+          {
+            ID: 'ECT.14006.6',
+            Value: 'AIS'
+          }
+        ];
+      translateResponse(customApiResponse, patientData);
+      const diagnosticReports = patientData.filter(pd => pd.resourceType === 'DiagnosticReport');
+
+      expect(diagnosticReports.length).to.equal(1);
+      expect(diagnosticReports.some(p => p.code.coding.some(coding => coding.code === '65753-6'))).to.be.true;
+      expect(diagnosticReports.some(p => p.conclusionCode.some(cC => cC.coding.some(coding => coding.code === '254890008')))).to.be.true;
+    });
+  });
+
+  describe('Parse histologic cancer excision results', () => {
+    it('should map a histologic cancer excision result to a biopsy', function () {
+      customApiResponse.Order[0].ExcisionResults = 
+        [
+          {
+            ID: 'ECT.14006.11',
+            Value: 'Adenocarcinoma'
+          }
+        ];      
+      translateResponse(customApiResponse, patientData);
+      const diagnosticReports = patientData.filter(pd => pd.resourceType === 'DiagnosticReport');
+
+      expect(diagnosticReports.length).to.equal(1);
+      expect(diagnosticReports.some(p => p.code.coding.some(coding => coding.code === '65753-6'))).to.be.true;
+      expect(diagnosticReports.some(p => p.conclusionCode.some(cC => cC.coding.some(coding => coding.code === '363354003')))).to.be.true;
     });
   });
 });
