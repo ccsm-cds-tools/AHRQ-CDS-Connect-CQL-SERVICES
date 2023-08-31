949a12767b1760fb2fb989b663a536790efe8d15
Mick O'Hanlon <mohanlon@mitre.org>
2023-08-31 13:20:12 -0400

    Refine Prefetch (#21)
    * Refine prefetch parameters to only those needed by UPMC
    
    * Update DiagnosticReport.search prefetch field to only search for code rather than codesystem+code

--- config/apply/ccsm/prefetch.js
+++ config/apply/ccsm/prefetch.js
@@ -1,15 +1,12 @@
 export const prefetch = {
   'Patient': 'Patient/{{context.patientId}}',
-  'ObservationCore': 'Observation?patient={{context.patientId}}&category=core-characteristics',
-  'ObservationLab': 'Observation?patient={{context.patientId}}&category=laboratory',
-  'ObservationLabor': 'Observation?patient={{context.patientId}}&category=labor-delivery',
-  'ObservationObGyn': 'Observation?patient={{context.patientId}}&category=obstetrics-gynecology',
-  'ObservationSmart': 'Observation?patient={{context.patientId}}&category=smartdata',
-  'Condition': 'Condition?patient={{context.patientId}}',
+  'ConditionEncounterDiagnosis': 'Condition?patient={{context.patientId}}&category=encounter-diagnosis&clinical-status=active',
   'ConditionProblem': 'Condition?patient={{context.patientId}}&category=problem-list-item&clinical-status=active',
-  'MedicationRequest': 'MedicationRequest?patient={{context.patientId}}',
-  'Procedure': 'Procedure?patient={{context.patientId}}',
-  'DiagnosticReport': 'DiagnosticReport?patient={{context.patientId}}',
-  'Encounter': 'Encounter?patient={{context.patientId}}',
-  'Immunization': 'Immunization?patient={{context.patientId}}'
+  'ConditionMedicalHistory': 'Condition?patient={{context.patientId}}&category=medical-history&clinical-status=active',
+  'MedicationRequest': 'MedicationRequest?patient={{context.patientId}}&status=active&intent=order',
+  'ProcedureOrders': 'Procedure?patient={{context.patientId}}&category=103693007',
+  'ProcedureSurgerical': 'Procedure?patient={{context.patientId}}&category=387713003',
+  'DiagnosticReport': 'DiagnosticReport?patient={{context.patientId}}&category=LAB',
+  'Encounter': 'Encounter/{{context.encounterId}}',
+  'Immunization': 'Immunization?patient={{context.patientId}}&status=completed'
 };
