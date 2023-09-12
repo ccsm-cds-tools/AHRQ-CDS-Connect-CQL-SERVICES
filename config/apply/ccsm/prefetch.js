d42fdfbdee02e1b7fb12a7876b4894a8bcc05e97
Mick O'Hanlon <mohanlon@mitre.org>
2023-09-12 02:46:26 -0400

    Modify prefetch: remove ConditionEncounterDiagnosis, remove MedicationRequest, remove Immunization, filter DiagnosticReport by category=CYTOPATH. (#22)

--- config/apply/ccsm/prefetch.js
+++ config/apply/ccsm/prefetch.js
@@ -1,12 +1,9 @@
 export const prefetch = {
   'Patient': 'Patient/{{context.patientId}}',
-  'ConditionEncounterDiagnosis': 'Condition?patient={{context.patientId}}&category=encounter-diagnosis&clinical-status=active',
   'ConditionProblem': 'Condition?patient={{context.patientId}}&category=problem-list-item&clinical-status=active',
   'ConditionMedicalHistory': 'Condition?patient={{context.patientId}}&category=medical-history&clinical-status=active',
-  'MedicationRequest': 'MedicationRequest?patient={{context.patientId}}&status=active&intent=order',
   'ProcedureOrders': 'Procedure?patient={{context.patientId}}&category=103693007',
   'ProcedureSurgerical': 'Procedure?patient={{context.patientId}}&category=387713003',
-  'DiagnosticReport': 'DiagnosticReport?patient={{context.patientId}}&category=LAB',
+  'DiagnosticReport': 'DiagnosticReport?patient={{context.patientId}}&category=CYTOPATH',
   'Encounter': 'Encounter/{{context.encounterId}}',
-  'Immunization': 'Immunization?patient={{context.patientId}}&status=completed'
 };
