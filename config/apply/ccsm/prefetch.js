ff04d1ca86f723dcc28a9e403d7c827a0b1296be
Mick O'Hanlon <mohanlon@mitre.org>
2023-12-19 13:17:53 -0500

    Remove prefetch queries to look for immunocompromised data - specifically Observation.Search and Condition.Search

--- config/apply/ccsm/prefetch.js
+++ config/apply/ccsm/prefetch.js
@@ -1,7 +1,5 @@
 export const prefetch = {
   'Patient': 'Patient/{{context.patientId}}',
-  'Condition': 'Condition?patient={{context.patientId}}&category=problem-list-item,medical-history&clinical-status=active',
-  'Procedure': 'Procedure?patient={{context.patientId}}&category=103693007,387713003',
   'Encounter': 'Encounter/{{context.encounterId}}',
   'EpisodeOfCare': 'EpisodeOfCare?patient={{context.patientId}}&type=urn:oid:1.2.840.114350.1.13.88.2.7.2.726668|6,urn:oid:1.2.840.114350.1.13.88.3.7.2.726668|6',
 };
