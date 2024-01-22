7fc31f2c8055b531efaf2628ac187442dbbe0816
Mick O'Hanlon <mohanlon@mitre.org>
2024-01-21 23:58:37 -0500

    Remove non-production EpisodeOfCare type from prefetch search query, since only one type is allowed to be searched. (#33)

--- config/apply/ccsm/prefetch.js
+++ config/apply/ccsm/prefetch.js
@@ -1,5 +1,5 @@
 export const prefetch = {
   'Patient': 'Patient/{{context.patientId}}',
   'Encounter': 'Encounter/{{context.encounterId}}',
-  'EpisodeOfCare': 'EpisodeOfCare?patient={{context.patientId}}&type=urn:oid:1.2.840.114350.1.13.88.2.7.2.726668|6,urn:oid:1.2.840.114350.1.13.88.3.7.2.726668|6',
+  'EpisodeOfCare': 'EpisodeOfCare?patient={{context.patientId}}&type=urn:oid:1.2.840.114350.1.13.88.2.7.2.726668|6',
 };
