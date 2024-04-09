1db2b659230b6a3fb8fc97bba9ae836433999ecd
Mike Nosal <mnosal@mitre.org>
2024-04-09 16:25:00 -0400

    Update config/apply/ccsm/collapse.js
    Co-authored-by: Mick O'Hanlon <mohanlon@mitre.org>

--- config/apply/ccsm/collapse.js
+++ config/apply/ccsm/collapse.js
@@ -163,7 +163,7 @@ const table = pug.compile(
   '\t\t\ttd(style={"border-bottom":"1px solid #eee","padding":"5px 20px 5px 0","width":"8em"}) #{val.date ? dayjs(val.date).format("MM/DD/YYYY") : "n/a"}\n' +
   '\t\t\ttd(style={"border-bottom":"1px solid #eee","padding":"5px 20px 5px 0","width":"14em"}) #{val.name}\n' +
   '\t\t\ttd(style={"border-bottom":"1px solid #eee","padding":"5px 20px 5px 0"}) #{val.value || "n/a"}'
-)
+);
   
 function formatEntry(ent) {
   return table({history:ent,dayjs:dayjs});
